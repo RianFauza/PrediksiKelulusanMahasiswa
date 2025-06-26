85928b6214a6c12625b7f58fe596fc46d80b2407
RianFauza <115771479+RianFauza@users.noreply.github.com>
2025-06-26 01:25:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ if "welcome_animasi" not in st.session_state:
     """, height=450)
 
     # Menandakan animasi sudah selesai
-    time.sleep(2)
+    time.sleep(1)
     st.session_state.welcome_animasi = False
-    st.experimental_rerun()  # Meminta aplikasi untuk me-refresh dan melanjutkan ke beranda
+    st.stop()
 
 # Animasi balon awal saat pertama kali membuka aplikasi
 if "welcome_balloon" not in st.session_state:
